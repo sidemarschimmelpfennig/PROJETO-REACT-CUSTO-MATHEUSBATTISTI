dff7532510057bd49f2d8acfb13c02ae198da61b
Sidemar Schimmelpfennig Junior <90000000+sidemarschimmelpfennig@users.noreply.github.com>
2022-04-10 17:40:19 -0300

    react ajuste

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -3,10 +3,9 @@ import styles from "../project/ProjectCard.module.css"
 
 function ServiceCard({id, name, cost, description, handleRemove}) {
     
-    const remove = (e) => {
+   const remove = (e) => {
         e.preventDefault();
         handleRemove(id, cost);
-
     }
 
   return(
